adb592e9c7d5c0f5ac9c4caa1acb52d32560e912
Nailya <nubtehy@gmail.com>
2018-11-29 16:36:10 +0200

    Added news names to tests

--- test/specs/products/CartItemQuantity.spec.js
+++ test/specs/products/CartItemQuantity.spec.js
@@ -2,9 +2,11 @@
 import Category from 'page-objects/Category';
 import Cart from 'page-objects/Cart';
 
-Category.open();
 
 describe('Given the Category page is opened', () => {
+  beforeEach(() => {
+    Category.open();
+  });
   describe('when add to cart button is clicked', () => {
     beforeEach(() => {
       Category.findbuttonsAddtoCart.forEach((link) => {
@@ -15,13 +17,12 @@ describe('Given the Category page is opened', () => {
 
 
     describe('Given the cart page is opened', () => {
-      describe('when plus button is clicked', () => {
-        beforeEach(() => {
-          Cart.findbuttonsPlus.forEach((link) => {
-            console.log(link, 'LINK');
-            link.click();
-          });
+      beforeEach(() => {
+        Cart.findbuttonsPlus.forEach((link) => {
+          link.click();
         });
+      });
+      describe('when plus button is clicked', () => {
         it('should add quantity 2 to product in cart', () => {
           expect(Cart.findCartProductQuantity('Item 1').getText()).toMatch('-2+');
           expect(Cart.findCartProductQuantity('Item 2').getText()).toMatch('-2+');
---
+++ test/specs/products/CheckConfirmaytion.spec.js
@@ -0,0 +1,38 @@
+
+import Category from 'page-objects/Category';
+import Cart from 'page-objects/Cart';
+import Confirmation from 'page-objects/Confirmation';
+
+
+describe('Given the Category page is opened', () => {
+  beforeEach(() => {
+    Category.open();
+  });
+  describe('when add to cart button is clicked', () => {
+    beforeEach(() => {
+      Category.findbuttonsAddtoCart.forEach((link) => {
+        link.click();
+      });
+      browser.element('header').click('a*=Cart');
+    });
+
+
+    describe('when confirm button is clicked', () => {
+      beforeEach(() => {
+        Confirmation.inputUserName.setValue('Anna');
+        Confirmation.inputUserAddress.setValue('Some st, 12/2');
+      });
+      describe('when confirm button is clicked', () => {
+        beforeEach(() => {
+          browser.pause(12000);
+          Confirmation.buttonConfirmation.click();
+        });
+        it('should show 4 products om confirmation page and user info', () => {
+          expect(Cart.findCartProductQuantity('Item 1').getText()).toMatch('Item 1');
+          expect(Confirmation.userInfoName.getText()).toMatch('Anna');
+          expect(Confirmation.userInfoAddress.getText()).toMatch('Some st, 12/2');
+        });
+      });
+    });
+  });
+});
